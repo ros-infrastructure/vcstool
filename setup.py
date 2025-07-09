19cb2684db3dd56347509c2e4a1cd9a136f2504c
Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
2025-07-09 22:35:02 +0100

    Rename package and add long description from README

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
+"""Setup script for the vcs2l package."""
+
+import os
+
 from setuptools import find_packages
 from setuptools import setup
 from vcstool import __version__
 
+with open(
+    os.path.join(os.path.dirname(__file__), 'README.rst'),
+        'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
-    name='vcstool',
+    name='vcs2l',
     version=__version__,
+    requires_python='>=3.7',
     install_requires=[
         'PyYAML',
         'setuptools'],
@@ -20,20 +30,25 @@ setup(
     packages=find_packages(),
     author='Dirk Thomas',
     author_email='web@dirk-thomas.net',
-    maintainer='Dirk Thomas',
-    maintainer_email='web@dirk-thomas.net',
-    url='https://github.com/dirk-thomas/vcstool',
-    download_url='http://download.ros.org/downloads/vcstool/',
-    classifiers=['Intended Audience :: Developers',
+    maintainer='ROS Infrastructure Team',
+    project_urls={
+        'Source code':
+        'https://github.com/ros-infrastructure/vcs2l',
+        'Issue tracker':
+        'https://github.com/ros-infrastructure/vcs2l/issues',
+    },
+    url='https://github.com/ros-infrastructure/vcs2l',
+    keywords=['vcs', 'version control', 'git', 'hg', 'svn', 'bzr'],
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'Intended Audience :: Developers',
                  'License :: OSI Approved :: Apache Software License',
                  'Programming Language :: Python',
                  'Topic :: Software Development :: Version Control',
                  'Topic :: Utilities'],
-    description='vcstool provides a command line tool to invoke vcs commands '
+    description='vcs2l provides a command line tool to invoke vcs commands '
         'on multiple repositories.',
-    long_description='\
-vcstool enables batch commands on multiple different vcs repositories. \
-Currently it supports git, hg, svn and bzr.',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
     license='Apache License, Version 2.0',
     data_files=[
         ('share/vcs2l-completion', [
